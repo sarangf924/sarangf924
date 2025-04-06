3e2896443f3cef62a43d8a0ef10dc48819d25599
sarangf924 <lakeeshasandani@gmail.com>
2025-04-06 12:39:26 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-/*const map = new Map();
+const map = new Map();
 const data = [
     ["https://www.bilibili.tv/en/video/4794375606243328?bstar_from=bstar-web.homepage.recommend.all", "https://raw.githubusercontent.com/sarangf924/sarangf924/refs/heads/main/BR/1.srt"],
     ["key2", "value2"],
@@ -6,6 +6,5 @@ const data = [
     // Add more key-value pairs as needed
 ];
 
-data.forEach(([key, value]) => map.set(key, value));*/ // Add all pairs at once
+data.forEach(([key, value]) => map.set(key, value)); // Add all pairs at once
 //console.log(map.get(window.location.href)); // Outputs: "value1"
-console.log("im loaded successfully")
