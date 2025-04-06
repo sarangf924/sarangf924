21ca566ffc3ea3f3b3f404b38c836919b285cafe
sarangf924 <lakeeshasandani@gmail.com>
2025-04-06 13:08:25 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const map = new Map();
+map = new Map();
 const data = [
     ["https://www.bilibili.tv/en/video/4794375606243328?bstar_from=bstar-web.homepage.recommend.all", "https://raw.githubusercontent.com/sarangf924/sarangf924/refs/heads/main/BR/1.srt"],
     ["key2", "value2"],
